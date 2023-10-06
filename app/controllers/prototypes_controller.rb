259befddeb5c31c511b4065267a23f5d1e29489d
kazuki1597 <144293988+kazuki1597@users.noreply.github.com>
2023-10-06 17:43:38 +0900

    isee編集３

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new,:create,:update,:destroy,:edit]
   before_action :set_prototype, only: [:show,:edit,:update,:destroy]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
 
 
     def index
